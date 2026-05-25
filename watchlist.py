6a5ffdc9b0a69e4c0fbb9b79d5afefb5bd5f0a20
Jesica <Jesica.bermudes11@gmail.com>
2026-05-24 21:58:12 -0400

    added setup functions

--- watchlist.py
+++ watchlist.py
@@ -1,4 +1,35 @@
+def add_movie():
+    pass
+
+def remove_movie():
+    pass
+
+def mark_watched():
+    pass
+
+def show_watchlist():
+    pass
+
+
 def main():
-    print("Hello")
-    
+    movie = {}
+    watchlist = []
+
+    while True:
+        print(f"\n🎬 Our Watchlist")
+        print(f"=================")
+
+
+        print(f"1. Add a movie")
+        print(f"2. Remove a movie")
+        print(f"3. Mark as watched")
+        print(f"4. Show watchlist")
+        print(f"5. Quit")
+
+        option = int(input("Select an option: "))
+
+        if option == 5:
+            break
+
+
 main()
