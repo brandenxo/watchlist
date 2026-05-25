bab92c4d1a76b16d7ef33220b1f396b2340e3106
brandenxo <catmeow1738@gmail.com>
2026-05-24 22:34:23 -0400

    my changes

--- watchlist.py
+++ watchlist.py
@@ -1,5 +1,11 @@
-def add_movie():
-    pass
+def add_movie(watchlist):
+    movie = {}
+
+    title = input("Enter movie title: ")
+    movie["title"] = title
+    watchlist.append(movie)
+
+    return watchlist
 
 def remove_movie():
     pass
@@ -12,13 +18,11 @@ def show_watchlist():
 
 
 def main():
-    movie = {}
     watchlist = []
 
     while True:
         print(f"\n🎬 Our Watchlist")
         print(f"=================")
-
 
         print(f"1. Add a movie")
         print(f"2. Remove a movie")
@@ -28,7 +32,16 @@ def main():
 
         option = int(input("Select an option: "))
 
-        if option == 5:
+        if option == 1:
+            watchlist = add_movie(watchlist)
+            print(watchlist[0]["title"])
+        elif option == 2:
+            remove_movie()
+        elif option == 3:
+            mark_watched()
+        elif option == 4:
+            show_watchlist()
+        elif option == 5:
             break
 
 
